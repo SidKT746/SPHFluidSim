5ad301aed1520ad69cf73be9f1ca9e5b827d7e3d
SidKT746 <tyagi.kshitij746@gmail.com>
2023-08-16 08:23:42 +0100

    ParticleClass Not being implemented correctly

---
+++ Particle.py
@@ -0,0 +1,6 @@
+class Particle():
+    def __init__(self, position, radius, colour):
+        self.position = position
+        self.radius = radius
+        self.colour = colour
+        self.velocity = 0
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # Importing the Taichi module to be able to compile the Python code
 import taichi as ti
 
+# Importing the Particle class
+from Particle import Particle
+
 # Initialising Taichi and telling it to use the GPU rather than the CPU.
 ti.init(arch=ti.gpu)
+
+Particles = []
 
 # Set up the basic variables like the time difference (dt), number of substeps in the simulation.
 n = 128
@@ -22,32 +27,50 @@ canvas.set_background_color((0, 0, 0))
 scene = ti.ui.Scene()
 camera = ti.ui.Camera()
 
-verticalLines = [ti.Vector.field(3, dtype=ti.f32, shape=2) for x in range(4)]
-horizontalLines = [ti.Vector.field(3, dtype=ti.f32, shape=2) for x in range(4)]
+# Creating a field for all the horizontal and vertical lines that define the domain of the fluid.
+VerticalLines = [ti.Vector.field(3, dtype=ti.f32, shape=2) for x in range(4)]
+HorizontalLines = [ti.Vector.field(3, dtype=ti.f32, shape=2) for x in range(4)]
+
+# Set the co-ordinates of the lines so that the domain can be seen.
 
 
 @ti.kernel
-def init_points_pos():
-    verticalLines[0][0] = [10, 0, 0]
-    verticalLines[0][1] = [10, 10, 0]
-    verticalLines[1][0] = [5, 0, 0]
-    verticalLines[1][1] = [5, 10, 0]
-    verticalLines[2][0] = [10, 0, -5]
-    verticalLines[2][1] = [10, 10, -5]
-    verticalLines[3][0] = [5, 0, -5]
-    verticalLines[3][1] = [5, 10, -5]
+def InitPointsPos(Particles: list[]):
+    VerticalLines[0][0] = [10, 0, 0]
+    VerticalLines[0][1] = [10, 100, 0]
+    VerticalLines[1][0] = [5, 0, 0]
+    VerticalLines[1][1] = [5, 100, 0]
+    VerticalLines[2][0] = [10, 0, -5]
+    VerticalLines[2][1] = [10, 100, -5]
+    VerticalLines[3][0] = [5, 0, -5]
+    VerticalLines[3][1] = [5, 100, -5]
 
-    horizontalLines[0][0] = [10, 0, 0]
-    horizontalLines[0][1] = [5, 0, 0]
-    horizontalLines[1][0] = [5, 0, 0]
-    horizontalLines[1][1] = [5, 0, -5]
-    horizontalLines[2][0] = [5, 0, -5]
-    horizontalLines[2][1] = [10, 0, -5]
-    horizontalLines[3][0] = [10, 0, -5]
-    horizontalLines[3][1] = [10, 0, 0]
+    HorizontalLines[0][0] = [10, 0, 0]
+    HorizontalLines[0][1] = [5, 0, 0]
+    HorizontalLines[1][0] = [5, 0, 0]
+    HorizontalLines[1][1] = [5, 0, -5]
+    HorizontalLines[2][0] = [5, 0, -5]
+    HorizontalLines[2][1] = [10, 0, -5]
+    HorizontalLines[3][0] = [10, 0, -5]
+    HorizontalLines[3][1] = [10, 0, 0]
+
+    InitParticles(Particles)
 
 
-init_points_pos()
+InitPointsPos(Particles)
+
+
+@ti.func
+def InitParticles(Particles):
+    # Setting up the properties of the ball
+    # The center of the ball is a vector and contained in a datatype called a vector field of floats.
+    ParticleRadius = 0.02
+
+    for i in range(1000):
+        ParticleCenter = ti.Vector.field(3, dtype=float, shape=(1,))
+        ParticleCenter[0] = [0.02 * (i * 1000//250), 1000 // 250, 1000 // 250]
+        Particles.append(Particle(ParticleCenter[0], colour=(0.0, 0.0, 1)))
+
 
 # Initialize the current time to be 0 (i.e. simulation starts now)
 current_t = 0.0
@@ -66,10 +89,14 @@ while window.running:
     scene.particles(ball_center, radius=ball_radius *
                     0.95, color=(0.0, 0.0, 1))
 
+    for i in Particles:
+        # Draw the Sphere.
+        scene.particles(i.position, radius=i.radius, color=i.colour)
+
     # Draw the line between the two points
     for i in range(4):
-        scene.lines(verticalLines[i], color=(0.28, 0.68, 0.99), width=5.0)
-        scene.lines(horizontalLines[i], color=(0.28, 0.68, 0.99), width=5.0)
+        scene.lines(VerticalLines[i], color=(0.28, 0.68, 0.99), width=5.0)
+        scene.lines(HorizontalLines[i], color=(0.28, 0.68, 0.99), width=5.0)
 
     # Set the Scene to the new scene with the ball and line in it.
     canvas.scene(scene)
